3e80ca346b7821813b6a02d848fb36b72363d5bf
Pedro Ivo <pi3virgula14@outlook.com>
2021-11-18 00:24:20 -0300

    remove reorganization

--- backend/crawlers/blockListener.js
+++ backend/crawlers/blockListener.js
@@ -84,17 +84,17 @@ const crawler = async () => {
 
             if (res && res.rows.length > 0) {
                 // Chain reorganization detected! We need to update block_author, block_hash and state_root
-                logger.debug(loggerOptions, `Detected chain reorganization at block #${blockNumber}, updating author, author name, hash and state root`);
-                const blockAuthor = extendedHeader.author;
-                const blockAuthorIdentity = await api.derive.accounts.info(blockAuthor);
-                const blockAuthorName = getDisplayName(blockAuthorIdentity.identity);
-                sql = `UPDATE block
-                       SET block_author      = '${blockAuthor}',
-                           block_author_name = '${blockAuthorName}',
-                           block_hash        = '${blockHash}',
-                           state_root        = '${stateRoot}'
-                       WHERE block_number = '${blockNumber}'`;
-                res = await dbQuery(client, sql, loggerOptions);
+                // logger.debug(loggerOptions, `Detected chain reorganization at block #${blockNumber}, updating author, author name, hash and state root`);
+                // const blockAuthor = extendedHeader.author;
+                // const blockAuthorIdentity = await api.derive.accounts.info(blockAuthor);
+                // const blockAuthorName = getDisplayName(blockAuthorIdentity.identity);
+                // sql = `UPDATE block
+                //        SET block_author      = '${blockAuthor}',
+                //            block_author_name = '${blockAuthorName}',
+                //            block_hash        = '${blockHash}',
+                //            state_root        = '${stateRoot}'
+                //        WHERE block_number = '${blockNumber}'`;
+                // res = await dbQuery(client, sql, loggerOptions);
             } else {
                 const blockAuthor = extendedHeader.author || '';
                 const [
